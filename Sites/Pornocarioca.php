800ee8cf6cb3f4410e6a7ce19e012459be4c2f72
Joseph Felix <joseph0xfff@gmail.com>
2017-10-02 22:06:58 -0300

    Aplicado identação da PSR-2 no site exemplo

--- Sites/Pornocarioca.php
+++ Sites/Pornocarioca.php
@@ -3,52 +3,52 @@ namespace Pornbot\Sites;
 
 class Pornocarioca extends \Pornbot\Core\Sitebase
 {
-	public function name()
-	{
-		return 'pornocarioca.com';
-	}
-	
-	public function type()
-	{
-		return 'html';
-	}
+    public function name()
+    {
+        return 'pornocarioca.com';
+    }
 
-	public function url()
-	{
-		return 'http://www.pornocarioca.com/';
-	}
-	
-	public function title()
-	{
-		return [
-			'pattern' => '/<title>([^\<]+)<\/title>/i',
-            'regex' => true
-		];
-	}
-	
-	public function duration()
-	{
-		return [
-			'pattern' => '/(\d{2}\:\d{2})\s</',
-            'regex' => true
-		];
-	}
-	
-	public function thumbnail()
-	{
-		return [
-			'pattern' => 'meta[itemprop="thumbnailUrl image"]',
-			'attr' => 'content',
-            'regex' => false
-		];
-	}
-	
-	public function link()
-	{
-		return [
-			'pattern' => 'li.list-item a',
-			'attr' => 'href',
-            'regex' => false
-		];
-	}
+    public function type()
+    {
+        return 'html';
+    }
+
+    public function url()
+    {
+        return 'http://www.pornocarioca.com/';
+    }
+
+    public function title()
+    {
+        return [
+            'pattern' => '/<title>([^\<]+)<\/title>/i',
+            'regex'   => true
+        ];
+    }
+
+    public function duration()
+    {
+        return [
+            'pattern' => '/(\d{2}\:\d{2})\s</',
+            'regex'   => true
+        ];
+    }
+
+    public function thumbnail()
+    {
+        return [
+            'pattern' => 'meta[itemprop="thumbnailUrl image"]',
+            'attr'    => 'content',
+            'regex'   => false
+        ];
+    }
+
+    public function link()
+    {
+        return [
+            'pattern' => 'li.list-item a',
+            'attr'    => 'href',
+            'regex'   => false
+        ];
+    }
 }
